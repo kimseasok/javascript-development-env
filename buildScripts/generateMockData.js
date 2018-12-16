92fa55b342fe19fec55d0eba9b8b7c335d04bf78
kimseasok <basicblogtalk@gmail.com>
2018-12-15 22:04:21 -0500

    Add delete user

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -9,8 +9,8 @@ const json = JSON.stringify(jsf(schema));
 
 fs.writeFile("./src/api/db.json", json, (err) => {
   if (err) {
-    return console.log(chalk.red(err));
+    return console.log(chalk.red(err)); // eslint-disable-line no-console
   } else {
-    console.log(chalk.green("Mock data generated"));
+    console.log(chalk.green("Mock data generated")); // eslint-disable-line no-console
   }
 });
