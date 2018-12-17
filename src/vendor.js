e10253239dc801459be0350e215d647fb592d1e5
kimseasok <basicblogtalk@gmail.com>
2018-12-16 22:57:28 -0500

    config splite bundle

---
+++ src/vendor.js
@@ -0,0 +1,6 @@
+/* The commemt will be bundling in the vendor.js file.
+I'm not sure you're gonna see this comment, but if you would this is gonna be awesome. */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,16 +6,23 @@ export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
+
+    // Use commonsChunkPlugin to create a separate bundle
+    // of vendor libraries so they're cache separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     //Create HTML file that reference to bundle JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
